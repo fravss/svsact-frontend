161fcf230e8ec8abd599ef06e6dc9954765fbec2
fravss <anaflaviamartins56@gmail.com>
2025-02-03 11:43:20 -0300

    tranformar os metodos em assincronos

--- src/app/services/denuncia.service.ts
+++ src/app/services/denuncia.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
 import { environment } from '../../environments/environment.development';
 import { Denuncia } from '../interfaces/denuncia';
 
@@ -13,32 +13,39 @@ export class DenunciaService {
 
   constructor(private http: HttpClient) { }
 
-  getDenuncia(): Observable<any> {
-    console.log("no get den")
-    return this.http.get<any>(this.baseUrl);
+  async getDenuncia(): Promise<any> {
+    console.log("no get den");
+    return await firstValueFrom(this.http.get<any>(this.baseUrl));
   }
-  getDenunciaById(id: number): Observable<any> {
-    return this.http.get<any>(`${this.baseUrl}/${id}`);
+
+  async getDenunciaById(id: number): Promise<any> {
+    return await firstValueFrom(this.http.get<any>(`${this.baseUrl}/${id}`));
   }
-  deleteDenuncia(id: number): Observable<any> {
-    return this.http.delete<any>(`${this.baseUrl}/${id}`);
+
+  async deleteDenuncia(id: number): Promise<any> {
+    return await firstValueFrom(this.http.delete<any>(`${this.baseUrl}/${id}`));
   }
-  addDenuncia(denuncia: Denuncia) : Observable<Denuncia>{
-    console.log(denuncia)
-    return this.http.post<Denuncia>(`${this.baseUrl}`, denuncia);
+
+  async addDenuncia(denuncia: Denuncia): Promise<Denuncia> {
+    console.log(denuncia);
+    return await firstValueFrom(this.http.post<Denuncia>(`${this.baseUrl}`, denuncia));
   }
-  updateDenuncia(denuncia: Denuncia, id: number) : Observable<Denuncia>{
-    return this.http.put<Denuncia>(`${this.baseUrl}/${id}`, denuncia);
+
+  async updateDenuncia(denuncia: Denuncia, id: number): Promise<Denuncia> {
+    return await firstValueFrom(this.http.put<Denuncia>(`${this.baseUrl}/${id}`, denuncia));
   }
-  getOrigemDenuncias(): Observable<string[]> {
-    return this.http.get<string[]>(`${this.baseUrl}/origem-denuncia`);
+
+  async getOrigemDenuncias(): Promise<string[]> {
+    return await firstValueFrom(this.http.get<string[]>(`${this.baseUrl}/origem-denuncia`));
   }
-  getDireitosViolados(): Observable<string[]> {
-    return this.http.get<string[]>(`${this.baseUrl}/direito-violado`);
+
+  async getDireitosViolados(): Promise<string[]> {
+    return await firstValueFrom(this.http.get<string[]>(`${this.baseUrl}/direito-violado`));
   }
-  getAgenteViolador(): Observable<string[]> {
+
+  async getAgenteViolador(): Promise<string[]> {
     console.log('aqui');
-    return this.http.get<string[]>(`${this.baseUrl}/agente-violador`);
+    return await firstValueFrom(this.http.get<string[]>(`${this.baseUrl}/agente-violador`));
   }
   
   
