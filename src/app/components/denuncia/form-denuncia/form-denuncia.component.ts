3c7c87931686d050718b6241d26af380df577881
fravss <anaflaviamartins56@gmail.com>
2025-02-03 12:23:04 -0300

    add pipe para formatar data

--- src/app/components/denuncia/form-denuncia/form-denuncia.component.ts
+++ src/app/components/denuncia/form-denuncia/form-denuncia.component.ts
@@ -1,17 +1,19 @@
-import { Component,  OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Validators } from '@angular/forms';
 import { DenunciaService } from '../../../services/denuncia.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { StatusRD } from '../../../interfaces/enums/StatusRD';
 import { FormComponent } from '../../shared/form/form.component';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-form-denuncia',
-  imports:  [FormComponent],
+  imports: [FormComponent],
   templateUrl: './form-denuncia.component.html',
   styleUrl: './form-denuncia.component.scss',
+  providers:[DatePipe]
 })
-export class FormDenunciaComponent  implements OnInit {
+export class FormDenunciaComponent implements OnInit {
   formConfig: any[] = [];
   denunciaId?: number;
   editValues? = {};
@@ -20,38 +22,46 @@ export class FormDenunciaComponent  implements OnInit {
     private denunciaService: DenunciaService,
     private router: Router,
     private route: ActivatedRoute,
-   
-  ) {}
+    private datePipe: DatePipe,
 
- async ngOnInit(): Promise<void> {   
-  this.formConfig = [
-    { name: 'responsaveis', label: 'Responsáveis', type: 'text', validators: [Validators.required] },
-    { name: 'criancasAdolescentes', label: 'Crianças ou Adolescentes Envolvidos', type: 'text', validators: [Validators.required] },
-    { name: 'relato', label: 'Relato', type: 'textarea', validators: [Validators.required] },
-    { name: 'medidasAplicadas', label: 'Medidas Aplicadas', type: 'text', validators: [Validators.required] },
-    { name: 'dataEmissao', label: 'Data de Emissão', type: 'date', validators: [Validators.required] },
-    { name: 'origemDenuncia', label: 'Origem Denúncia', type: 'select', options: [], validators: [Validators.required] },
-    { name: 'statusRD', label: 'Status', type: 'select', options: Object.values(StatusRD).map(status => ({ value: status, label: status })), validators: [Validators.required] }
-  ];
+  ) { }
+
+  async ngOnInit(): Promise<void> {
+    this.formConfig = [
+      { name: 'responsaveis', label: 'Responsáveis', type: 'text', validators: [Validators.required] },
+      { name: 'criancasAdolescentes', label: 'Crianças ou Adolescentes Envolvidos', type: 'text', validators: [Validators.required] },
+      { name: 'relato', label: 'Relato', type: 'textarea', validators: [Validators.required] },
+      { name: 'medidasAplicadas', label: 'Medidas Aplicadas', type: 'text', validators: [Validators.required] },
+      { name: 'dataEmissao', label: 'Data de Emissão', type: 'date', validators: [Validators.required] },
+      { name: 'origemDenuncia', label: 'Origem Denúncia', type: 'select', options: [], validators: [Validators.required] },
+      { name: 'statusRD', label: 'Status', type: 'select', options: Object.values(StatusRD).map(status => ({ value: status, label: status })), validators: [Validators.required] }
+    ];
     try {
-  
+      await this.populateOptions();
+
       this.denunciaId = Number(this.route.snapshot.paramMap.get('id'));
       if (this.denunciaId) {
-        const denunciaData = await this.denunciaService.getDenunciaById(this.denunciaId);
-        this.formConfig = this.formConfig.map(field => ({
-          ...field,
-          value: denunciaData[field.name] || field.value
-        }));
-       }
-      const origemDenunciaData = await this.denunciaService.getOrigemDenuncias();
-      const origemDenunciaField = this.formConfig.find(field => field.name === 'origemDenuncia');
-      if (origemDenunciaField) {
-        origemDenunciaField.options = origemDenunciaData.map(origem => ({ value: origem, label: origem }));
+        await this.onEdit(this.denunciaId);
       }
-     
     } catch (error) {
       console.error('Erro ao carregar dados:', error);
     }
+  }
+
+  async onEdit(id: number): Promise<void> {
+    const denunciaData = await this.denunciaService.getDenunciaById(id);
+  
+    this.formConfig = this.formConfig.map(field => ({
+      ...field,
+      value: field.type === 'date' ? this.datePipe.transform(denunciaData[field.name], 'yyyy-MM-dd')
+        : denunciaData[field.name]
+    }));
+  }
+  
+  async populateOptions(): Promise<void> {
+    const origemDenunciaData = await this.denunciaService.getOrigemDenuncias();
+    const origemDenunciaField = this.formConfig.find(field => field.name === 'origemDenuncia');
+    origemDenunciaField.options = origemDenunciaData.map(origem => ({ value: origem, label: origem }));
   }
 
   async onSubmit(formValue: any): Promise<void> {
